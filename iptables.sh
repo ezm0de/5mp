0e3a93e95a5f1f5be70bea2ba83da5bdce263f94
BradHacker <harkerbd@gmail.com>
2022-04-11 16:59:48 -0400

    Flush iptables first

--- iptables.sh
+++ iptables.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+sudo iptables -t raw -F
+sudo iptables -t raw -X
+sudo iptables -t mangle -F
+sudo iptables -t mangle -X
+sudo iptables -t nat -F
+sudo iptables -t nat -X
+sudo iptables -t filter -F
+sudo iptables -t filter -X
+sudo iptables -F
+sudo iptables -X
+
 sudo bash iptables-ssh.sh
 sudo bash iptables-http.sh
 sudo bash iptables-deny.sh
